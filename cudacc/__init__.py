ae5b16061d1b88a46488bd33366014927592fdfa
fanrado <radofana@gmail.com>
2026-03-01 11:33:48 -0500

    export the function accelerate

--- cudacc/__init__.py
+++ cudacc/__init__.py
@@ -6,5 +6,8 @@ Public API entry point.
 
 from .accelerator import accelerate
 
+# Import bridges to trigger self-registration
+from . import bridges
+
 __version__ = "0.1.0"
 __all__ = ["accelerate"]
